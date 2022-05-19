a2565ab1b6a8ca954b7f013caed88eb5c1975e04
Paul Jeandel <pauljeandel@hotmail.com>
2022-05-20 00:55:27 +0200

    sss

--- run.sh
+++
@@ -1,9 +0,0 @@
-export FLASK_APP=main
-flask run
-pip install aaaaaaa
-pip install bbbbbbb
-pip install ccccccc
-pip install ddddddd
-pip install eeeeeee
-pip install fffffff
-pip install kfjdkfhdfhkjdshfjdhfjkdhfkjsd
